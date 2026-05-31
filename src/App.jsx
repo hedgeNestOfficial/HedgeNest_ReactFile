c96e35039bafeb80f86fd031c95d1b0fd034808f
davies ruth <daviesruth82@gmail.com>
2026-05-31 21:43:35 +0100

    header and footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import Button from "./Components/Button";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HeroPage from "./Pages/HeroPage";
+
+
+
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HeroPage />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+   
+    </>
+    
   );
 };
 
--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "../Css/Button.css";
 
 const Button = (props) => {
-  return <button className={props.className}>{props.children}</button>;
+  return <button className={props.className}>{props.text}</button>;
 };
 
 export default Button;
