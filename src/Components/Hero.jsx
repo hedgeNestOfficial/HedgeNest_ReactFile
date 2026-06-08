db9466fec90c804f1fafffa73f499e185aa28ecc
Pr-ElTech <empero.lehnzy42@gmail.com>
2026-06-08 07:54:34 +0100

    fixing reposiveness on hero sections and working on navigation for resetpass

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
             <span className="highlight-text">Grow Confidently,</span> <br />
             Save Smarter.
           </h1>
-          <p className="hero-description">
+          <p className="hero-description-context1">
             HedgeNest helps everyday Nigerians protect their money from
             inflation, build better saving habits, and grow their wealth
             securely.
