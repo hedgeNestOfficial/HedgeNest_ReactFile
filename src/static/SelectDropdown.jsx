b51c7e3c707b0829dd47bcb517d02fdff0cfbcaa
Pr-ElTech <empero.lehnzy42@gmail.com>
2026-06-23 10:09:54 +0100

    fixing style

--- src/static/SelectDropdown.jsx
+++ src/static/SelectDropdown.jsx
@@ -1,17 +1,27 @@
-import React, { useState } from "react";
+import React from "react";
 import { LuChevronDown } from "react-icons/lu";
 import "../Style/SelectDropdown.css";
 
-const SelectDropdown = ({ data }) => {
-  const [isOpen, setIsOpen] = useState(false);
-
+const SelectDropdown = ({
+  data,
+  isOpen,
+  isMobile,
+  onToggle,
+  onMouseEnter,
+  onMouseLeave,
+}) => {
   return (
     <div
-      className="select-dropdown-wrapper"
-      onMouseEnter={() => setIsOpen(true)}
-      onMouseLeave={() => setIsOpen(false)}
+      className={`select-dropdown-wrapper ${isOpen ? "is-open" : ""}`}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
     >
-      <div className="select-dropdown-header">
+      {/* Header action executes click toggle context conditionally */}
+      <div
+        className="select-dropdown-header"
+        onClick={onToggle}
+        style={{ cursor: isMobile ? "pointer" : "default" }} // Keeps design intent clean
+      >
         <h3>{data.Question}</h3>
 
         <LuChevronDown
@@ -19,11 +29,9 @@ const SelectDropdown = ({ data }) => {
         />
       </div>
 
-      {isOpen && (
-        <div className="select-dropdown-body">
-          <p>{data.answer}</p>
-        </div>
-      )}
+      <div className="select-dropdown-body">
+        <p>{data.answer}</p>
+      </div>
     </div>
   );
 };
